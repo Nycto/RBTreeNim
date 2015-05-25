73e7c701f11a90e2557aeda2bc000d702205a398
Nycto <James@RoundEights.com>
2015-05-25 12:35:16 -0700

    Support for inserting a single node

--- rbtree.nim
+++ rbtree.nim
@@ -32,18 +32,31 @@ proc newRBTree*[T](): RedBlackTree[T] =
 proc `$` [T]( self: Node[T] ): string =
     ## Converts a node to a string
     if self == nil:
-        return "()"
+        result = "()"
     else:
-        return "(%s: %s)" % [
-            if self.color == red: "R" else: "B",
-            $(self.value),
-            if self.left != nil or self.right != nil:
-                " " & $(self.left) & " " & $(self.right)
-            else:
-                ""
-        ]
+        result = "(" &
+            (if self.color == red: "R" else: "B") & " " &
+            $(self.value)
+        if self.left != nil or self.right != nil:
+            result.add(" " & $(self.left) & " " & $(self.right))
+        result.add(")")
 
 proc `$`* [T]( self: RedBlackTree[T] ): string =
     ## Returns a tree as a string
     return "RedBlackTree" & `$`[T](self.root)
+
+proc newNode[T]( value: T, parent: Node[T] = nil ): Node[T] =
+    return Node[T](
+        left: nil, right: nil, parent: parent, color: red, value: value )
+
+proc insertCase1[T]( node: var Node[T] ) =
+    ## Case 1: The current node N is at the root of the tree
+    if node.parent == nil:
+        node.color = black
+
+proc insert*[T]( self: var RedBlackTree[T], value: T ) =
+    ## Adds a value to this tree
+    if self.root == nil:
+        self.root = newNode(value)
+        insertCase1(self.root)
 
--- test/rbtree_test.nim
+++ test/rbtree_test.nim
@@ -1,8 +1,17 @@
-import unittest, rbtree
+import unittest, rbtree, sequtils
+
+proc `==`[T]( actual: RedBlackTree[T], expected: string ): bool =
+    $actual == expected
 
 suite "A Red/Black Tree should":
 
     test "Instantiate as an empty tree":
         let tree = newRBTree[int]()
-        require( $tree == "RedBlackTree()" )
+        require( tree == "RedBlackTree()" )
+
+    test "Insert nodes":
+        var tree = newRBTree[int]()
+
+        tree.insert(1)
+        require( tree == "RedBlackTree(B 1)" )
 
